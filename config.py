6ff44695458706545a88e4e6f55aab7288612116
AtaBy4vir <75300684+AtaBy4vir@users.noreply.github.com>
2021-07-27 14:38:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-token = "Token"
-admins = [] # TR: Buraya idni arkadaşlarının idsini yazabilirsin EN: You can add your friends account id or your id to this array 
+token = "ODY5NTM4ODQ0MzU2MjgwMzIx.YP_rJw.qq4hRf7JLKZjG381GLpxy0LQKdA"
+admins = [672357759974506496] # TR: Buraya idni arkadaşlarının idsini yazabilirsin EN: You can add your friends account id or your id to this array 
